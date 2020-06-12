7731b7f4f8a7f7b6939e2c625f8daa8f1b9150f9
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-12 18:43:35 +0200

    with spring initializr generated spring boot project template

---
+++ src/main/java/io/horrorshow/soulswap/SoulSwapApplication.java
@@ -0,0 +1,13 @@
+package io.horrorshow.soulswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulSwapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/horrorshow/soulswap/SoulSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.horrorshow.soulswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
