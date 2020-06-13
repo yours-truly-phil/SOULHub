67de96b2e86816987b6ec64598e528a9ade82979
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-13 17:39:47 +0200

    created proto soul patch endpoint

---
+++ src/main/java/io/horrorshow/soulswap/SOULPatchEndpoint.java
@@ -0,0 +1,31 @@
+package io.horrorshow.soulswap;
+
+import io.horrorshow.soulswap.soulswap.SoulswapRequest;
+import io.horrorshow.soulswap.soulswap.SoulswapResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class SOULPatchEndpoint {
+    private static final String NAMESPACE_URI = "http://soulswap.horrorshow.io/soulswap";
+
+    private SOULSwapRepository soulSwapRepository;
+
+    @Autowired
+    public SOULPatchEndpoint(SOULSwapRepository soulSwapRepository) {
+        this.soulSwapRepository = soulSwapRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "soulswapRequest")
+    @ResponsePayload
+    public SoulswapResponse getSoulswapResponse(@RequestPayload SoulswapRequest request) {
+        SoulswapResponse response = new SoulswapResponse();
+        response.getSoulpatch().add(
+                soulSwapRepository.findSOULPatch(
+                        request.getSoulpatchId().stream().toString()));
+        return response;
+    }
+}
