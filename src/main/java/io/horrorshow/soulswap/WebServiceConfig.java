1958994d3a6a645a2c7ba5af4942e78525aa60fc
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-13 17:52:20 +0200

    created webservice and wsdl servlet at /ws/soulpatches.wsdl

---
+++ src/main/java/io/horrorshow/soulswap/WebServiceConfig.java
@@ -0,0 +1,41 @@
+package io.horrorshow.soulswap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "soulpatches")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema soulswapSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("SoulSwapPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://soulswap.horrorshow.io/soulswap");
+        wsdl11Definition.setSchema(soulswapSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema soulswapSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/soulswap.xsd"));
+    }
+}
