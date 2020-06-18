819a65f1295b8fc8d9c3e47ed6e4115fe8b5d726
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-18 13:15:24 +0200

    added hibernate search dependencies and marked soulpatch for indexing

--- src/main/java/io/horrorshow/soulswap/data/SOULPatch.java
+++ src/main/java/io/horrorshow/soulswap/data/SOULPatch.java
@@ -3,11 +3,14 @@ package io.horrorshow.soulswap.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Indexed
 @Table(name = "soulpatches")
 @Data
 @AllArgsConstructor
@@ -17,17 +20,30 @@ public class SOULPatch {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @NotBlank
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String name;
+
     @Column(columnDefinition = "TEXT")
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String description;
+
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String soulFileName;
+
     @Column(columnDefinition = "TEXT")
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String soulFileContent;
+
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String soulpatchFileName;
+
     @Column(columnDefinition = "TEXT")
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String soulpatchFileContent;
-    @NotBlank
+
     private String author;
+
     private Long noServings;
 }
