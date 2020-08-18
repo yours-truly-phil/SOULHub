5ead78c0015d86cfe8a9ff0324da5f8f8f43a44f
Philipp Seeger <philippbseeger@googlemail.com>
2020-08-18 20:12:31 +0200

    added add soulpatch button to navbar for logged in users

--- src/main/java/io/horrorshow/soulhub/ui/components/AppUserNavBarComponent.java
+++ src/main/java/io/horrorshow/soulhub/ui/components/AppUserNavBarComponent.java
@@ -1,9 +1,14 @@
 package io.horrorshow.soulhub.ui.components;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import io.horrorshow.soulhub.data.AppUser;
 import io.horrorshow.soulhub.ui.UIConst;
+import io.horrorshow.soulhub.ui.views.EditSOULPatchView;
 
 import java.util.Optional;
 
@@ -12,6 +17,10 @@ public class AppUserNavBarComponent extends HorizontalLayout {
     private static final long serialVersionUID = 833982187716488558L;
 
     public AppUserNavBarComponent(AppUser user) {
+        Button addSOULPatch = new Button("add SOULPatch", VaadinIcon.FILE_ADD.create());
+        addSOULPatch.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        addSOULPatch.addClickListener(e -> UI.getCurrent().navigate(EditSOULPatchView.class, "new"));
+        add(addSOULPatch);
 
         Anchor toUserInfoView = new Anchor(UIConst.ROUTE_USERINFO + "/" + user.getUserName(), user.getUserName());
         add(toUserInfoView);
