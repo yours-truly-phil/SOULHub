d2bdfb88d2e71ac25112d9755ca1b214e1fa7590
Philipp Seeger <philippbseeger@googlemail.com>
2020-08-18 20:04:15 +0200

    added add soulpatch btn to navbar

--- src/main/java/io/horrorshow/soulhub/ui/components/LoginNavBarComponent.java
+++ src/main/java/io/horrorshow/soulhub/ui/components/LoginNavBarComponent.java
@@ -1,10 +1,15 @@
 package io.horrorshow.soulhub.ui.components;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
 import io.horrorshow.soulhub.ui.UIConst;
+import io.horrorshow.soulhub.ui.views.EditSOULPatchView;
 import io.horrorshow.soulhub.ui.views.RegistrationView;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +21,7 @@ public class LoginNavBarComponent extends HorizontalLayout {
     private final Anchor confirm = new Anchor(UIConst.ROUTE_CONFIRM_REGISTER, UIConst.LINK_TEXT_CONFIRM_REGISTER);
     private final RouterLink registration =
             new RouterLink(UIConst.LINK_TEXT_REGISTER, RegistrationView.class);
+    private final Button addSOULPatch = new Button("add SOULPatch", VaadinIcon.FILE_ADD.create());
 
     public LoginNavBarComponent() {
         init();
@@ -28,10 +34,14 @@ public class LoginNavBarComponent extends HorizontalLayout {
     }
 
     private void initComponents() {
+        addSOULPatch.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        addSOULPatch.addClickListener(e -> UI.getCurrent().navigate(EditSOULPatchView.class, "new"));
+
         registration.setHighlightCondition(HighlightConditions.sameLocation());
     }
 
     private void arrangeComponents() {
+        add(addSOULPatch);
         add(confirm);
         add(login);
         add(registration);
