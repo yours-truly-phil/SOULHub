cfc684012cfb6457a2dfe0a76e7ebe7fa64f6f52
Philipp Seeger <philippbseeger@googlemail.com>
2020-09-03 12:24:01 +0200

    get httpservletrequest url

--- src/main/java/io/horrorshow/soulhub/ui/util/UIUtils.java
+++ src/main/java/io/horrorshow/soulhub/ui/util/UIUtils.java
@@ -1,11 +1,23 @@
 package io.horrorshow.soulhub.ui.util;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class UIUtils {
     public static void updateURI(String uri) {
         UI.getCurrent()
                 .getPage()
                 .executeJs("history.pushState(history.state,'','" + uri + "');");
+    }
+
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((VaadinServletRequest) VaadinService.getCurrentRequest()).getHttpServletRequest();
+    }
+
+    public static StringBuffer getRequestUrl() {
+        return getHttpServletRequest().getRequestURL();
     }
 }
