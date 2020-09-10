7ef1c6dbd63312b2385877937ae868a88ca32a21
Philipp Seeger <philippbseeger@googlemail.com>
2020-09-10 20:59:14 +0200

    automatic redirect from http to https if property soulhub.redirect-https = true

--- src/main/java/io/horrorshow/soulhub/SOULHubApplication.java
+++ src/main/java/io/horrorshow/soulhub/SOULHubApplication.java
@@ -1,17 +1,61 @@
 package io.horrorshow.soulhub;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class SOULHubApplication {
+
+    @Value("${soulhub.http-port:8080}")
+    int httpPort;
+    @Value("${server.port:8443}")
+    int httpsPort;
 
     public static void main(final String[] args) {
         ApplicationContext context = SpringApplication.run(SOULHubApplication.class, args);
 
         System.out.println(context.getApplicationName() + "SOULHub Application started!");
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+            value = "soulhub.redirect-https",
+            havingValue = "true"
+    )
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
     }
 
 }
