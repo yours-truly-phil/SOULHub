0a002b6de70c035aa14dc303caac17175409c354
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-22 14:26:38 +0100

    Use Spring 2.3.7 and Vaadin 14.4.4

--- docker_run_postgres.sh
+++ docker_run_postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 -v /home/phil/docker/volumes/postgres:/var/lib/postgresql/data postgres
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 -v /home/phil/docker/volumes/postgres:/var/lib/postgresql/data postgres:12
 
